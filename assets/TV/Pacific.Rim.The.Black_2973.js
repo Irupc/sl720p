80fcf49b9c0997946fd31295d82b6a7a759ca123
Irupc <68991705+Irupc@users.noreply.github.com>
2021-03-06 16:48:24 +0530

    Create Pacific.Rim.The.Black_2973.js

---
+++ assets/TV/Pacific.Rim.The.Black_2973.js
@@ -0,0 +1,18 @@
+lastRes = "720p"; lastSize = "150MB"; lastRip = "WebRip"; lastMsg1 = "Warning! This is x265 (HEVC) Codec"; lastMsg2 = "Some <b>Televisions</b> may not be able to play this video";
+s01Res = "720p"; s01Size = "~120MB"; s01Rip = "WebRip"; s01Msg1 = "Warning! This is x265 (HEVC) Codec"; s01Msg2 = "Some <b>Televisions</b> may not be able to play this video";
+
+s01e01x = "https://cloud.irupc.xyz/0:/TV.Shows/Pacific.Rim.The.Black/iruPC.net_Pacific.Rim.The.Black.S01E01.x265.Dual.Aud.%5BJpn+Eng%5D.720p.10bit.WEBRip.2CH.HEVC-s.mkv";
+s01e02x = "https://cloud.irupc.xyz/0:/TV.Shows/Pacific.Rim.The.Black/iruPC.net_Pacific.Rim.The.Black.S01E02.x265.Dual.Aud.%5BJpn+Eng%5D.720p.10bit.WEBRip.2CH.HEVC-f.mkv";
+s01e03x = "https://cloud.irupc.xyz/0:/TV.Shows/Pacific.Rim.The.Black/iruPC.net_Pacific.Rim.The.Black.S01E03.x265.Dual.Aud.%5BJpn+Eng%5D.720p.10bit.WEBRip.2CH.HEVC-g.mkv";
+s01e04x = "https://cloud.irupc.xyz/0:/TV.Shows/Pacific.Rim.The.Black/iruPC.net_Pacific.Rim.The.Black.S01E04.x265.Dual.Aud.%5BJpn+Eng%5D.720p.10bit.WEBRip.2CH.HEVC-d.mkv";
+s01e05x = "https://cloud.irupc.xyz/0:/TV.Shows/Pacific.Rim.The.Black/iruPC.net_Pacific.Rim.The.Black.S01E05.x265.Dual.Aud.%5BJpn+Eng%5D.720p.10bit.WEBRip.2CH.HEVC-l.mkv";
+s01e06x = "https://cloud.irupc.xyz/0:/TV.Shows/Pacific.Rim.The.Black/iruPC.net_Pacific.Rim.The.Black.S01E06.x265.Dual.Aud.%5BJpn+Eng%5D.720p.10bit.WEBRip.2CH.HEVC-k.mkv";
+s01e07x = "https://cloud.irupc.xyz/0:/TV.Shows/Pacific.Rim.The.Black/iruPC.net_Pacific.Rim.The.Black.S01E07.x265.Dual.Aud.%5BJpn+Eng%5D.720p.10bit.WEBRip.2CH.HEVC-o.mkv";
+
+s01e01y = "https://dl.irupc.xyz/431734821526620/Pacific_Rim_The_Black_S01E01_From_the_Shadows_DUAL_AUDIO_JPN_ENG.mkv";
+s01e02y = "https://dl.irupc.xyz/431752001395804/Pacific_Rim_The_Black_S01E02_Into_the_Black_DUAL_AUDIO_JPN_ENG_720p.mkv";
+s01e03y = "https://dl.irupc.xyz/431769181264988/Pacific_Rim_The_Black_S01E03_Bogan_DUAL_AUDIO_JPN_ENG_720p_10bit.mkv";
+s01e04y = "https://dl.irupc.xyz/431786361134172/Pacific_Rim_The_Black_S01E04_Up_and_Running_DUAL_AUDIO_JPN_ENG_720p.mkv";
+s01e05y = "https://dl.irupc.xyz/431803541003356/Pacific_Rim_The_Black_S01E05_Escaping_Bogan_DUAL_AUDIO_JPN_ENG_720p.mkv";
+s01e06y = "https://dl.irupc.xyz/431820720872540/Pacific_Rim_The_Black_S01E06_Boneyard_DUAL_AUDIO_JPN_ENG_720p_10bit.mkv";
+s01e07y = "https://dl.irupc.xyz/431837900741724/Pacific_Rim_The_Black_S01E07_Showdown_DUAL_AUDIO_JPN_ENG_720p_10bit.mkv";
