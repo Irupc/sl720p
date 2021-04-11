dec6666c6eea3e96651f36d6338baec6b3c1fc1a
Irupc <68991705+Irupc@users.noreply.github.com>
2021-04-11 22:30:31 +0530

    Create Castlevania_12375.js

---
+++ assets/TV/Castlevania_12375.js
@@ -0,0 +1,19 @@
+lastRes = "720p"; lastSize = "80MB"; lastRip = "WebRip"; lastMsg1 = "Warning! This is x265 (HEVC) Codec"; lastMsg2 = "Some <b>Televisions</b> may not be able to play this video";
+s01Res = "720p"; s01Size = "~80MB"; s01Rip = "WebRip"; s01Msg1 = "Warning! This is x265 (HEVC) Codec"; s01Msg2 = "Some <b>Televisions</b> may not be able to play this video";
+s02Res = "720p"; s02Size = "~70MB"; s02Rip = "WebRip"; s02Msg1 = "Warning! This is x265 (HEVC) Codec"; s02Msg2 = "Some <b>Televisions</b> may not be able to play this video";
+s03Res = "720p"; s03Size = "~56MB"; s03Rip = "WebRip"; s03Msg1 = "Warning! This is x265 (HEVC) Codec"; s03Msg2 = "Some <b>Televisions</b> may not be able to play this video";
+
+//Season 1 URL
+s01e01x = "https://dl.irupc.xyz/519987809524828/Castlevania.S01E01.@cinemajunkies.720p.WEBRip.x265.PSA.mkv"; s01e02x = "https://dl.irupc.xyz/520004989394012/Castlevania.S01E02.@cinemajunkies.720p.WEBRip.x265.PSA.mkv"; s01e03x = "https://dl.irupc.xyz/520022169263196/Castlevania.S01E03.@cinemajunkies.720p.WEBRip.x265.PSA.mkv"; s01e04x = "https://dl.irupc.xyz/520039349132380/Castlevania.S01E04.@cinemajunkies.720p.WEBRip.x265.PSA.mkv";
+//@@01z@@  
+s01e01y = "@@01y@@"; s01e02y = "@@01y@@"; s01e03y = "@@01y@@"; s01e04y = "@@01y@@";
+
+//Season 2 URL
+s02e01x = "https://dl.irupc.xyz/520125248478300/Castlevania.S02E01.@cinemajunkies.720p.WEBRip.x265.PSA.mkv"; s02e02x = "https://dl.irupc.xyz/520142428347484/Castlevania.S02E02.@cinemajunkies.720p.WEBRip.x265.PSA.mkv"; s02e03x = "https://dl.irupc.xyz/520159608216668/Castlevania.S02E03.@cinemajunkies.720p.WEBRip.x265.PSA.mkv"; s02e04x = "https://dl.irupc.xyz/520176788085852/Castlevania.S02E04.@cinemajunkies.720p.WEBRip.x265.PSA.mkv"; s02e05x = "https://dl.irupc.xyz/520193967955036/Castlevania.S02E05.@cinemajunkies.720p.WEBRip.x265.PSA.mkv"; s02e06x = "https://dl.irupc.xyz/520211147824220/Castlevania.S02E06.@cinemajunkies.720p.WEBRip.x265.PSA.mkv"; s02e07x = "https://dl.irupc.xyz/520228327693404/Castlevania.S02E07.@cinemajunkies.720p.WEBRip.x265.PSA.mkv"; s02e08x = "https://dl.irupc.xyz/520245507562588/Castlevania.S02E08.@cinemajunkies.720p.WEBRip.x265.PSA.mkv";
+//@@02z@@  
+s02e01y = "@@02y@@"; s02e02y = "@@02y@@"; s02e03y = "@@02y@@"; s02e04y = "@@02y@@"; s02e05y = "@@02y@@"; s02e06y = "@@02y@@"; s02e07y = "@@02y@@"; s02e08y = "@@02y@@";
+
+//Season 3 URL
+s03e01x = "https://dl.irupc.xyz/520400126385244/Castlevania.S03E01.@cinemajunkies.720p.WEBRip.x265.PSA.mkv"; s03e02x = "https://dl.irupc.xyz/520417306254428/Castlevania.S03E02.@cinemajunkies.720p.WEBRip.x265.PSA.mkv"; s03e03x = "https://dl.irupc.xyz/520434486123612/Castlevania.S03E03.@cinemajunkies.720p.WEBRip.x265.PSA.mkv"; s03e04x = "https://dl.irupc.xyz/520451665992796/Castlevania.S03E04.@cinemajunkies.720p.WEBRip.x265.PSA.mkv"; s03e05x = "https://dl.irupc.xyz/520468845861980/Castlevania.S03E05.@cinemajunkies.720p.WEBRip.x265.PSA.mkv"; s03e06x = "https://dl.irupc.xyz/520486025731164/Castlevania.S03E06.@cinemajunkies.720p.WEBRip.x265.PSA.mkv"; s03e07x = "https://dl.irupc.xyz/520503205600348/Castlevania.S03E07.@cinemajunkies.720p.WEBRip.x265.PSA.mkv"; s03e08x = "https://dl.irupc.xyz/520520385469532/Castlevania.S03E08.@cinemajunkies.720p.WEBRip.x265.PSA.mkv"; s03e09x = "https://dl.irupc.xyz/520537565338716/Castlevania.S03E09.@cinemajunkies.720p.WEBRip.x265.PSA.mkv"; s03e10x = "https://dl.irupc.xyz/520554745207900/Castlevania.S03E10.@cinemajunkies.720p.WEBRip.x265.PSA.mkv";
+//@@03z@@  
+s03e01y = "@@03y@@"; s03e02y = "@@03y@@"; s03e03y = "@@03y@@"; s03e04y = "@@03y@@"; s03e05y = "@@03y@@"; s03e06y = "@@03y@@"; s03e07y = "@@03y@@"; s03e08y = "@@03y@@"; s03e09y = "@@03y@@"; s03e10y = "@@03y@@";
