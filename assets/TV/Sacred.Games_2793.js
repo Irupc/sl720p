d2aba59c4fb71e2780988b765eecf0a038bad6d8
Irupc <68991705+Irupc@users.noreply.github.com>
2021-04-18 19:14:41 +0530

    Create Sacred.Games_2793.js

---
+++ assets/TV/Sacred.Games_2793.js
@@ -0,0 +1,13 @@
+lastRes = "720p"; lastSize = "290MB"; lastRip = "WebRip"; lastMsg1 = "Warning! This is x265 (HEVC) Codec"; lastMsg2 = "Some <b>Televisions</b> may not be able to play this video";
+s01Res = "720p"; s01Size = "~240MB"; s01Rip = "WebRip"; s01Msg1 = "Warning! This is x265 (HEVC) Codec"; s01Msg2 = "Some <b>Televisions</b> may not be able to play this video";
+s02Res = "720p"; s02Size = "~250MB"; s02Rip = "WebRip"; s02Msg1 = "Warning! This is x265 (HEVC) Codec"; s02Msg2 = "Some <b>Televisions</b> may not be able to play this video";
+
+//Season 1 URL
+s01e01x = "https://dl.irupc.xyz/530415990119516/Sacred.Games.S01E01.HIN-ENG.720p.2CH.x265.HEVC-PSA.mkv"; s01e02x = "https://dl.irupc.xyz/530467529727068/Sacred.Games.S01E02.HIN-ENG.720p.2CH.x265.HEVC-PSA.mkv"; s01e03x = "https://dl.irupc.xyz/530347270642780/Sacred.Games.S01E03.HIN-ENG.720p.2CH.x265.HEVC-PSA.mkv"; s01e04x = "https://dl.irupc.xyz/530398810250332/Sacred.Games.S01E04.HIN-ENG.720p.2CH.x265.HEVC-PSA.mkv"; s01e05x = "https://dl.irupc.xyz/530450349857884/Sacred.Games.S01E05.HIN-ENG.720p.2CH.x265.HEVC-PSA.mkv"; s01e06x = "https://dl.irupc.xyz/530364450511964/Sacred.Games.S01E06.HIN-ENG.720p.2CH.x265.HEVC-PSA.mkv"; s01e07x = "https://dl.irupc.xyz/530381630381148/Sacred.Games.S01E07.HIN-ENG.720p.2CH.x265.HEVC-PSA.mkv"; s01e08x = "https://dl.irupc.xyz/530433169988700/Sacred.Games.S01E08.HIN-ENG.720p.2CH.x265.HEVC-PSA.mkv";
+//@@01z@@  
+s01e01y = "@@01y@@"; s01e02y = "@@01y@@"; s01e03y = "@@01y@@"; s01e04y = "@@01y@@"; s01e05y = "@@01y@@"; s01e06y = "@@01y@@"; s01e07y = "@@01y@@"; s01e08y = "@@01y@@";
+
+//Season 2 URL
+s02e01x = "https://dl.irupc.xyz/530725227764828/Sacred.Games.S02E01.HIN-EN.720p.10bit.2CH.x265.HEVC-PSA.mkv"; s02e02x = "https://dl.irupc.xyz/530622148549724/Sacred.Games.S02E02.HIN-EN.720p.10bit.2CH.x265.HEVC-PSA.mkv"; s02e03x = "https://dl.irupc.xyz/530639328418908/Sacred.Games.S02E03.HIN-EN.720p.10bit.2CH.x265.HEVC-PSA.mkv"; s02e04x = "https://dl.irupc.xyz/530690868026460/Sacred.Games.S02E04.HIN-EN.720p.10bit.2CH.x265.HEVC-PSA.mkv"; s02e05x = "https://dl.irupc.xyz/530673688157276/Sacred.Games.S02E05.HIN-EN.720p.10bit.2CH.x265.HEVC-PSA.mkv"; s02e06x = "https://dl.irupc.xyz/530656508288092/Sacred.Games.S02E06.HIN-EN.720p.10bit.2CH.x265.HEVC-PSA.mkv"; s02e07x = "https://dl.irupc.xyz/530708047895644/Sacred.Games.S02E07.HIN-EN.720p.10bit.2CH.x265.HEVC-PSA.mkv"; s02e08x = "https://dl.irupc.xyz/530742407634012/Sacred.Games.S02E08.HIN-EN.720p.10bit.2CH.x265.HEVC-PSA.mkv";
+//@@02z@@  
+s02e01y = "@@02y@@"; s02e02y = "@@02y@@"; s02e03y = "@@02y@@"; s02e04y = "@@02y@@"; s02e05y = "@@02y@@"; s02e06y = "@@02y@@"; s02e07y = "@@02y@@"; s02e08y = "@@02y@@";
