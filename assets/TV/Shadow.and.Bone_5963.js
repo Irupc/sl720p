1cc490f57b88827ff019c720d5c76af627758527
Irupc <68991705+Irupc@users.noreply.github.com>
2021-04-26 10:42:40 +0530

    Create Shadow.and.Bone_5963.js

---
+++ assets/TV/Shadow.and.Bone_5963.js
@@ -0,0 +1,21 @@
+lastRes = "720p"; lastSize = "220MB"; lastRip = "WebRip"; lastMsg1 = "Warning! This is x265 (HEVC) Codec"; lastMsg2 = "Some <b>Televisions</b> may not be able to play this video";
+s01Res = "720p"; s01Size = "~210MB"; s01Rip = "WebRip"; s01Msg1 = "Warning! This is x265 (HEVC) Codec"; s01Msg2 = "Some <b>Televisions</b> may not be able to play this video";
+
+//Season 01 URL
+s01e01x = "https://series.irupc.xyz/0:/TV.Series/Shadow.and.Bone/S01/iruPC.net_Shadow.and.Bone.S01E01.A.Searing.Burst.of.Light.720p.10bit.WEBRip.mkv";
+s01e02x = "https://series.irupc.xyz/0:/TV.Series/Shadow.and.Bone/S01/iruPC.net_Shadow.and.Bone.S01E02.Were.All.Someones.Monster.720p.10bit_WEBRip.mkv";
+s01e03x = "https://series.irupc.xyz/0:/TV.Series/Shadow.and.Bone/S01/iruPC.net_Shadow.and.Bone.S01E03.The.Making.at.the.Heart.of.the.World.720p.mkv";
+s01e04x = "https://series.irupc.xyz/0:/TV.Series/Shadow.and.Bone/S01/iruPC.net_Shadow.and.Bone.S01E04.Otkazatsya.720p.10bit.WEBRip.2CH.x265.HEVC.mkv";
+s01e05x = "https://series.irupc.xyz/0:/TV.Series/Shadow.and.Bone/S01/iruPC.net_Shadow.and.Bone.S01E05.Show.Me.Who.You.Are.720p.10bit.WEBRip.2CH.mkv";
+s01e06x = "https://series.irupc.xyz/0:/TV.Series/Shadow.and.Bone/S01/iruPC.net_Shadow.and.Bone.S01E06.The.Heart.is.an.Arrow.720p.10bit.WEBRip.2CH.mkv";
+s01e07x = "https://series.irupc.xyz/0:/TV.Series/Shadow.and.Bone/S01/iruPC.net_Shadow.and.Bone.S01E07.The.Unsea.720p.10bit.WEBRip.2CH.x265.HEVC.mkv";
+s01e08x = "https://series.irupc.xyz/0:/TV.Series/Shadow.and.Bone/S01/iruPC.net_Shadow.and.Bone.S01E08.No.Mourners.720p.10bit.WEBRip.2CH.x265.HEVC.mkv";
+
+s01e01y = "https://dl.irupc.xyz/549777702689884/Shadow_and_Bone_S01E01_A_Searing_Burst_of_Light_720p_10bit_WEBRip.mkv";
+s01e02y = "https://dl.irupc.xyz/549794882559068/Shadow_and_Bone_S01E02_Were_All_Someones_Monster_720p_10bit_WEBRip.mkv";
+s01e03y = "https://dl.irupc.xyz/549812062428252/Shadow_and_Bone_S01E03_The_Making_at_the_Heart_of_the_World_720p.mkv";
+s01e04y = "https://dl.irupc.xyz/549829242297436/Shadow_and_Bone_S01E04_Otkazatsya_720p_10bit_WEBRip_2CH_x265_HEVC.mkv";
+s01e05y = "https://dl.irupc.xyz/549846422166620/Shadow_and_Bone_S01E05_Show_Me_Who_You_Are_720p_10bit_WEBRip_2CH.mkv";
+s01e06y = "https://dl.irupc.xyz/549863602035804/Shadow_and_Bone_S01E06_The_Heart_is_an_Arrow_720p_10bit_WEBRip_2CH.mkv";
+s01e07y = "https://dl.irupc.xyz/549897961774172/Shadow_and_Bone_S01E07_The_Unsea_720p_10bit_WEBRip_2CH_x265_HEVC.mkv";
+s01e08y = "https://dl.irupc.xyz/549880781904988/Shadow_and_Bone_S01E08_No_Mourners_720p_10bit_WEBRip_2CH_x265_HEVC.mkv";
