06ea4248b2202898c82f1d93ba4a3c131dad7c40
Irupc <68991705+Irupc@users.noreply.github.com>
2021-04-26 13:02:35 +0530

    Create Ben.10.Omniverse_Sinhala.265.js

---
+++ assets/TV/Ben.10.Omniverse_Sinhala.265.js
@@ -0,0 +1,93 @@
+lastRes = "360p"; lastSize = "108MB"; lastRip = "DVD"; lastMsg1 = "x264 Codec File"; lastMsg2 = "සිංහල Dubbed Episode";
+s01Res = "360p"; s01Size = "~110MB"; s01Rip = "DVD"; s01Msg1 = "x264 Codec File"; s01Msg2 = "සිංහල Dubbed Episode";
+s02Res = "360p"; s02Size = "~100MB"; s02Rip = "DVD"; s02Msg1 = "x264 Codec File"; s02Msg2 = "සිංහල Dubbed Episode";
+s03Res = "360p"; s03Size = "~100MB"; s03Rip = "DVD"; s03Msg1 = "x264 Codec File"; s03Msg2 = "සිංහල Dubbed Episode";
+s04Res = "360p"; s04Size = "~100MB"; s04Rip = "DVD"; s04Msg1 = "x264 Codec File"; s04Msg2 = "සිංහල Dubbed Episode";
+
+//Season 01 URL
+s01e01x = "https://series.irupc.xyz/0:/Sin.Dubbed/Ben.10.Omniverse/S01/iruPC.net_Ben.10.Omniverse.S01E01-r.mp4";
+s01e02x = "https://series.irupc.xyz/0:/Sin.Dubbed/Ben.10.Omniverse/S01/iruPC.net_Ben.10.Omniverse.S01E02-b.mp4";
+s01e03x = "https://No_Links";
+s01e04x = "https://series.irupc.xyz/0:/Sin.Dubbed/Ben.10.Omniverse/S01/iruPC.net_Ben.10.Omniverse.S01E04-q.mp4";
+s01e05x = "https://series.irupc.xyz/0:/Sin.Dubbed/Ben.10.Omniverse/S01/iruPC.net_Ben.10.Omniverse.S01E05-o.mp4";
+s01e06x = "https://No_Links";
+s01e07x = "https://series.irupc.xyz/0:/Sin.Dubbed/Ben.10.Omniverse/S01/iruPC.net_Ben.10.Omniverse.S01E07-q.mp4";
+s01e08x = "https://series.irupc.xyz/0:/Sin.Dubbed/Ben.10.Omniverse/S01/iruPC.net_Ben.10.Omniverse.S01E08-a.mp4";
+s01e09x = "https://No_Links";
+s01e10x = "https://series.irupc.xyz/0:/Sin.Dubbed/Ben.10.Omniverse/S01/iruPC.net_Ben.10.Omniverse.S01E10-n.mp4";
+
+s01e01y = "https://dl.irupc.xyz/548678191062108/EP1.mp4";
+s01e02y = "https://dl.irupc.xyz/548695370931292/S1%20-%20E2.mp4";
+s01e03y = "https://No_Links";
+s01e04y = "https://dl.irupc.xyz/548712550800476/EP4.mp4";
+s01e05y = "https://dl.irupc.xyz/548729730669660/Ep5.mp4";
+s01e06y = "https://No_Links";
+s01e07y = "https://dl.irupc.xyz/548746910538844/Ep7.mp4";
+s01e08y = "https://dl.irupc.xyz/548764090408028/EP8.mp4";
+s01e09y = "https://No_Links";
+s01e10y = "https://dl.irupc.xyz/548781270277212/EP10.mp4";
+
+//Season 02 URL
+s02e01x = "https://series.irupc.xyz/0:/Sin.Dubbed/Ben.10.Omniverse/S02/iruPC.net_Ben.10.Omniverse.S02E01-d.mp4";
+s02e02x = "https://series.irupc.xyz/0:/Sin.Dubbed/Ben.10.Omniverse/S02/iruPC.net_Ben.10.Omniverse.S02E02-q.mp4";
+s02e03x = "https://series.irupc.xyz/0:/Sin.Dubbed/Ben.10.Omniverse/S02/iruPC.net_Ben.10.Omniverse.S02E03-h.mp4";
+s02e04x = "https://series.irupc.xyz/0:/Sin.Dubbed/Ben.10.Omniverse/S02/iruPC.net_Ben.10.Omniverse.S02E04-f.mp4";
+s02e05x = "https://No_Links";
+s02e06x = "https://series.irupc.xyz/0:/Sin.Dubbed/Ben.10.Omniverse/S02/iruPC.net_Ben.10.Omniverse.S02E06-k.mp4";
+s02e07x = "https://series.irupc.xyz/0:/Sin.Dubbed/Ben.10.Omniverse/S02/iruPC.net_Ben.10.Omniverse.S02E07-s.mp4";
+s02e08x = "https://No_Links";
+s02e09x = "https://series.irupc.xyz/0:/Sin.Dubbed/Ben.10.Omniverse/S02/iruPC.net_Ben.10.Omniverse.S02E09-w.mp4";
+s02e10x = "https://series.irupc.xyz/0:/Sin.Dubbed/Ben.10.Omniverse/S02/iruPC.net_Ben.10.Omniverse.S02E10-c.mp4";
+
+s02e01y = "https://dl.irupc.xyz/548918709230684/S2E1.mp4";
+s02e02y = "https://dl.irupc.xyz/548935889099868/S2E2.mp4";
+s02e03y = "https://dl.irupc.xyz/548953068969052/S2E3.mp4";
+s02e04y = "https://dl.irupc.xyz/548970248838236/S2E4.mp4";
+s02e05y = "https://No_Links";
+s02e06y = "https://dl.irupc.xyz/548987428707420/S2E6.mp4";
+s02e07y = "https://dl.irupc.xyz/549004608576604/S2E07.mp4";
+s02e08y = "https://No_Links";
+s02e09y = "https://dl.irupc.xyz/549021788445788/S2E9.mp4";
+s02e10y = "https://dl.irupc.xyz/549038968314972/S2E10.mp4";
+
+//Season 03 URL
+s03e01x = "https://No_Links";
+s03e02x = "https://No_Links";
+s03e03x = "https://series.irupc.xyz/0:/Sin.Dubbed/Ben.10.Omniverse/S03/iruPC.net_Ben.10.Omniverse.S03E03-x.mp4";
+s03e04x = "https://series.irupc.xyz/0:/Sin.Dubbed/Ben.10.Omniverse/S03/iruPC.net_Ben.10.Omniverse.S03E04-b.mp4";
+s03e05x = "https://series.irupc.xyz/0:/Sin.Dubbed/Ben.10.Omniverse/S03/iruPC.net_Ben.10.Omniverse.S03E05-q.mp4";
+s03e06x = "https://series.irupc.xyz/0:/Sin.Dubbed/Ben.10.Omniverse/S03/iruPC.net_Ben.10.Omniverse.S03E05-q.mp4";
+s03e07x = "https://series.irupc.xyz/0:/Sin.Dubbed/Ben.10.Omniverse/S03/iruPC.net_Ben.10.Omniverse.S03E07-q.mp4";
+s03e08x = "https://series.irupc.xyz/0:/Sin.Dubbed/Ben.10.Omniverse/S03/iruPC.net_Ben.10.Omniverse.S03E08-t.mp4";
+s03e09x = "https://series.irupc.xyz/0:/Sin.Dubbed/Ben.10.Omniverse/S03/iruPC.net_Ben.10.Omniverse.S03E09-d.mp4";
+s03e10x = "https://series.irupc.xyz/0:/Sin.Dubbed/Ben.10.Omniverse/S03/iruPC.net_Ben.10.Omniverse.S03E10-a.mp4";
+
+s03e01y = "https://No_Links";
+s03e02y = "https://No_Links";
+s03e03y = "https://dl.irupc.xyz/549193587137628/S3E3.mp4";
+s03e04y = "https://dl.irupc.xyz/549210767006812/S3E4.mp4";
+s03e05y = "https://dl.irupc.xyz/549227946875996/S3E6.mp4";
+s03e06y = "https://dl.irupc.xyz/549245126745180/S3E5.mp4";
+s03e07y = "https://dl.irupc.xyz/549262306614364/S3E7.mp4";
+s03e08y = "https://dl.irupc.xyz/549279486483548/S3E8.mp4";
+s03e09y = "https://dl.irupc.xyz/549296666352732/S3E9.mp4";
+s03e10y = "https://dl.irupc.xyz/549313846221916/Ben%20se%20e10.mp4";
+
+//Season 04 URL
+s04e01x = "https://No_Links";
+s04e02x = "https://series.irupc.xyz/0:/Sin.Dubbed/Ben.10.Omniverse/S04/iruPC.net_Ben.10.Omniverse.S04E02-n.mp4";
+s04e03x = "https://series.irupc.xyz/0:/Sin.Dubbed/Ben.10.Omniverse/S04/iruPC.net_Ben.10.Omniverse.S04E03-b.mp4";
+s04e04x = "https://series.irupc.xyz/0:/Sin.Dubbed/Ben.10.Omniverse/S04/iruPC.net_Ben.10.Omniverse.S04E04-m.mp4";
+s04e05x = "https://series.irupc.xyz/0:/Sin.Dubbed/Ben.10.Omniverse/S04/iruPC.net_Ben.10.Omniverse.S04E05-q.mp4";
+s04e06x = "https://No_Links";
+s04e07x = "https://No_Links";
+s04e08x = "https://series.irupc.xyz/0:/Sin.Dubbed/Ben.10.Omniverse/S04/iruPC.net_Ben.10.Omniverse.S04E08-x.mp4";
+
+s04e01y = "https://No_Links";
+s04e02y = "https://dl.irupc.xyz/549468465044572/S4E2.mp4";
+s04e03y = "https://dl.irupc.xyz/549485644913756/S4E3.mp4";
+s04e04y = "https://dl.irupc.xyz/549502824782940/S4E4.mp4";
+s04e05y = "https://dl.irupc.xyz/549520004652124/S4E5.mp4";
+s04e06y = "https://No_Links";
+s04e07y = "https://No_Links";
+s04e08y = "https://dl.irupc.xyz/549537184521308/S4E8.mp4";
