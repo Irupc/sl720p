bb31aff73c75a6c3b780442941646f5309285c45
Irupc <68991705+Irupc@users.noreply.github.com>
2021-04-28 13:43:35 +0530

    Create Ginny.and.Georgia_2294.js

---
+++ assets/TV/Ginny.and.Georgia_2294.js
@@ -0,0 +1,25 @@
+lastRes = "720p"; lastSize = "426MB"; lastRip = "WebRip"; lastMsg1 = "Warning! This is x265 (HEVC) Codec"; lastMsg2 = "Some <b>Televisions</b> may not be able to play this video";
+s01Res = "720p"; s01Size = "~350MB"; s01Rip = "WebRip"; s01Msg1 = "Warning! This is x265 (HEVC) Codec"; s01Msg2 = "Some <b>Televisions</b> may not be able to play this video";
+
+//Season 01 URL
+s01e01x = "https://series.irupc.xyz/0:/TV.Series/Ginny.and.Georgia/S01/iruPC.net_iruPC.net_Ginny.and.Georgia.S01E01.720p.NF.WEBRip.x264-v.mkv";
+s01e02x = "https://series.irupc.xyz/0:/TV.Series/Ginny.and.Georgia/S01/iruPC.net_iruPC.net_Ginny.and.Georgia.S01E02.720p.NF.WEBRip.x264-n.mkv";
+s01e03x = "https://series.irupc.xyz/0:/TV.Series/Ginny.and.Georgia/S01/iruPC.net_iruPC.net_Ginny.and.Georgia.S01E03.720p.NF.WEBRip.x264-m.mkv";
+s01e04x = "https://series.irupc.xyz/0:/TV.Series/Ginny.and.Georgia/S01/iruPC.net_iruPC.net_Ginny.and.Georgia.S01E04.720p.NF.WEBRip.x264-q.mkv";
+s01e05x = "https://series.irupc.xyz/0:/TV.Series/Ginny.and.Georgia/S01/iruPC.net_iruPC.net_Ginny.and.Georgia.S01E05.720p.NF.WEBRip.x264-t.mkv";
+s01e06x = "https://series.irupc.xyz/0:/TV.Series/Ginny.and.Georgia/S01/iruPC.net_iruPC.net_Ginny.and.Georgia.S01E06.720p.NF.WEBRip.x264-u.mkv";
+s01e07x = "https://series.irupc.xyz/0:/TV.Series/Ginny.and.Georgia/S01/iruPC.net_iruPC.net_Ginny.and.Georgia.S01E07.720p.NF.WEBRip.x264-w.mkv";
+s01e08x = "https://series.irupc.xyz/0:/TV.Series/Ginny.and.Georgia/S01/iruPC.net_iruPC.net_Ginny.and.Georgia.S01E08.720p.NF.WEBRip.x264-r.mkv";
+s01e09x = "https://series.irupc.xyz/0:/TV.Series/Ginny.and.Georgia/S01/iruPC.net_iruPC.net_Ginny.and.Georgia.S01E09.720p.NF.WEBRip.x264-g.mkv";
+s01e10x = "https://series.irupc.xyz/0:/TV.Series/Ginny.and.Georgia/S01/iruPC.net_iruPC.net_Ginny.and.Georgia.S01E10.720p.NF.WEBRip.x264-g.mkv";
+
+s01e01y = "https://dl.irupc.xyz/553694712863836/Ginny.and.Georgia.S01E01.720p.NF.WEBRip.x264-GalaxyTV.mkv";
+s01e02y = "https://dl.irupc.xyz/553711892733020/Ginny.and.Georgia.S01E02.720p.NF.WEBRip.x264-GalaxyTV.mkv";
+s01e03y = "https://dl.irupc.xyz/553729072602204/Ginny.and.Georgia.S01E03.720p.NF.WEBRip.x264-GalaxyTV.mkv";
+s01e04y = "https://dl.irupc.xyz/553746252471388/Ginny.and.Georgia.S01E04.720p.NF.WEBRip.x264-GalaxyTV.mkv";
+s01e05y = "https://dl.irupc.xyz/553763432340572/Ginny.and.Georgia.S01E05.720p.NF.WEBRip.x264-GalaxyTV.mkv";
+s01e06y = "https://dl.irupc.xyz/553780612209756/Ginny.and.Georgia.S01E06.720p.NF.WEBRip.x264-GalaxyTV.mkv";
+s01e07y = "https://dl.irupc.xyz/553797792078940/Ginny.and.Georgia.S01E07.720p.NF.WEBRip.x264-GalaxyTV.mkv";
+s01e08y = "https://dl.irupc.xyz/553814971948124/Ginny.and.Georgia.S01E08.720p.NF.WEBRip.x264-GalaxyTV.mkv";
+s01e09y = "https://dl.irupc.xyz/553832151817308/Ginny.and.Georgia.S01E10.720p.NF.WEBRip.x264-GalaxyTV.mkv";
+s01e10y = "https://dl.irupc.xyz/553849331686492/Ginny.and.Georgia.S01E09.720p.NF.WEBRip.x264-GalaxyTV.mkv";
