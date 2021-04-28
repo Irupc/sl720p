f95a470a3462d358368cc38cbad26e9d47595987
Irupc <68991705+Irupc@users.noreply.github.com>
2021-04-28 08:48:06 +0530

    Create Brotherhood.Final.Fantasy.XV_2248.js

---
+++ assets/TV/Brotherhood.Final.Fantasy.XV_2248.js
@@ -0,0 +1,17 @@
+lastRes = "720p"; lastSize = "45MB"; lastRip = "HDTV"; lastMsg1 = "x264 Codec File"; lastMsg2 = "---";
+s01Res = "720p"; s01Size = "~75MB"; s01Rip = "HDTV"; s01Msg1 = "x264 Codec File"; s01Msg2 = "---";
+
+//Season 01 URL
+s01e01x = "https://series.irupc.xyz/0:/TV.Series/Brotherhood.Final.Fantasy.XV/iruPC.net_Brotherhood.Final.Fantasy.XV.Episode.1.i.Language.720p.x264.mkv";
+s01e02x = "https://series.irupc.xyz/0:/TV.Series/Brotherhood.Final.Fantasy.XV/iruPC.net_Brotherhood.Final.Fantasy.XV.Episode.2.u.language.720p.x264.mkv";
+s01e03x = "https://series.irupc.xyz/0:/TV.Series/Brotherhood.Final.Fantasy.XV/iruPC.net_Brotherhood.Final.Fantasy.XV.Episode.3.n.language.720p.x264.mkv";
+s01e04x = "https://series.irupc.xyz/0:/TV.Series/Brotherhood.Final.Fantasy.XV/iruPC.net_Brotherhood.Final.Fantasy.XV.Episode.4.multi.language.720p.x264.mkv";
+s01e05x = "https://series.irupc.xyz/0:/TV.Series/Brotherhood.Final.Fantasy.XV/iruPC.net_Brotherhood.Final.Fantasy.XV.Episode.5.b.language.720p.x264.mkv";
+s01e06x = "https://series.irupc.xyz/0:/TV.Series/Brotherhood.Final.Fantasy.XV/iruPC.net_Brotherhood.Final.Fantasy.XV.E06.Bonus.1080p.JAP.Audio.2016.Web.mp4";
+
+s01e01y = "https://dl.irupc.xyz/552973158358108/Brotherhood_Final_Fantasy_XV_Episode_1_multi_language_s_247_0.mkv";
+s01e02y = "https://dl.irupc.xyz/552990338227292/Brotherhood_Final_Fantasy_XV_Episode_2_multi_language_s_247_0.mkv";
+s01e03y = "https://dl.irupc.xyz/553007518096476/Brotherhood_Final_Fantasy_XV_Episode_3_multi_language_s_247_0.mkv";
+s01e04y = "https://dl.irupc.xyz/553024697965660/Brotherhood_Final_Fantasy_XV_Episode_4_multi_language_s_247_0.mkv";
+s01e05y = "https://dl.irupc.xyz/553041877834844/Brotherhood_Final_Fantasy_XV_Episode_5_multi_language_s_247_0.mkv";
+s01e06y = "https://dl.irupc.xyz/553059057704028/Brotherhood_Final_Fantasy_XV,_E06_Bonus_1080p_JAP_Audio_2016_Web.mp4";
